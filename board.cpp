3ed09821587c67c3e81b93283f612e04363f0d24
Theodore Roque <troque@usc.edu>
2013-02-28 17:55:34 -0800

    added lines that say hello

--- board.cpp
+++ board.cpp
@@ -6,12 +6,12 @@
 #include <map>
 
 using namespace std;
-	
+
 Board::Board(int size, int numInitMoves, int seed) {
 
   setSize(size);
   srand(seed);
-  
+
   _tiles = new int[_size];
 
   for (int i=0; i<_size; i++) {
@@ -21,7 +21,7 @@ Board::Board(int size, int numInitMoves, int seed) {
   }
 
 //scramble the board
-int blank;	
+int blank;
 int mv;
 int i;
 int count=numInitMoves;
@@ -30,16 +30,16 @@ int dim = static_cast<int>(sqrt(_size));
 while(count != 0) {
 for (i=0; i< _size; i++) {
   //find blank tile (blank)
-  if ( _tiles[i] == 0 )  {  
+  if ( _tiles[i] == 0 )  {
      blank = i;
-     break; 
+     break;
   }
 }
   //cout << "blank = " << blank << endl;
   //generate random number
   mv = rand() % 4;
   //cout << "mv = " << mv << endl;
- 
+
  //move north
   int north = blank - dim;
   if (mv == 0 && north >= 0) {
@@ -65,7 +65,7 @@ for (i=0; i< _size; i++) {
 
     _tiles[blank] = _tiles[south];
     _tiles[south] = 0;
-  
+
   }
 
   //move east
@@ -81,7 +81,7 @@ for (i=0; i< _size; i++) {
 }
 
 Board::Board(const Board &b) {
-												
+
   _size = b.getSize();
   _tiles = b.getTiles();
 
@@ -101,16 +101,16 @@ int bcol = blank%dim;
 */
 
  for (int i=0; i<_size; i++) {
-  
-   if (_tiles[i] == tile) 
-     { current_tile = i; 
-       break; }     
+
+   if (_tiles[i] == tile)
+     { current_tile = i;
+       break; }
  }
 
  for (int i=0; i<_size; i++) {
 
    if (_tiles[i] == 0)
-     { blank = i; 
+     { blank = i;
        break; }
  }
 
@@ -129,33 +129,33 @@ int bcol = blank%dim;
  _tiles[blank] = _tiles[current_tile];
  _tiles[current_tile] = 0;
 
- 
+
 }
 
 void Board::potentialMoves(map<int, Board*> &moveMap) {
 
   //clear the map
   map<int,Board*>::iterator it;
-  for (it = moveMap.begin(); it != moveMap.end(); it++) {  
+  for (it = moveMap.begin(); it != moveMap.end(); it++) {
       moveMap.erase(it);
   }
 
-  Board *potmove =  new Board(_size,0,0);  
+  Board *potmove =  new Board(_size,0,0);
   potmove->setTiles(_tiles);
 
-/*    
+/*
   cout << " potmove " << endl;
   for (int i=0; i<_size; i++) { cout << potmove->getTiles()[i] << endl; }
 */
-  
-  int dim = static_cast<int>(sqrt(potmove->getSize()));  
+
+  int dim = static_cast<int>(sqrt(potmove->getSize()));
 
   //find blank
   int blank;
 
   for (int i=0; i<potmove->getSize();i++) {
 
-    if (potmove->getTiles()[i] == 0) { 
+    if (potmove->getTiles()[i] == 0) {
       blank = i;
       break;
 
@@ -167,7 +167,7 @@ void Board::potentialMoves(map<int, Board*> &moveMap) {
   if (north >= 0) {
     potmove = new Board(_size,0,0);
     potmove->setTiles(_tiles);
-    potmove->getTiles()[blank] = potmove->getTiles()[north];  
+    potmove->getTiles()[blank] = potmove->getTiles()[north];
     potmove->getTiles()[north] = 0;
     moveMap[potmove->getTiles()[blank]] = potmove;
   }
@@ -181,7 +181,7 @@ cout << *this << endl;
   if (blank%dim != 0) {
     potmove = new Board(_size,0,0);
     potmove->setTiles(_tiles);
-    potmove->getTiles()[blank] = potmove->getTiles()[west];  
+    potmove->getTiles()[blank] = potmove->getTiles()[west];
     potmove->getTiles()[west] = 0;
     moveMap[potmove->getTiles()[blank]] = potmove;
 //cout << "west = " << potmove->getTiles()[blank] << endl;
@@ -197,7 +197,7 @@ cout << *this << endl;
   if (south <_size) {
     potmove = new Board(_size,0,0);
     potmove->setTiles(_tiles);
-    potmove->getTiles()[blank] = potmove->getTiles()[south];  
+    potmove->getTiles()[blank] = potmove->getTiles()[south];
     potmove->getTiles()[south] = 0;
     moveMap[potmove->getTiles()[blank]] = potmove;
   }
@@ -210,7 +210,7 @@ cout << *this << endl;
   if (east%dim != 0) {
     potmove = new Board(_size,0,0);
     potmove->setTiles(_tiles);
-    potmove->getTiles()[blank] = potmove->getTiles()[east];  
+    potmove->getTiles()[blank] = potmove->getTiles()[east];
     potmove->getTiles()[east] = 0;
     moveMap[potmove->getTiles()[blank]] = potmove;
   }
@@ -222,8 +222,15 @@ cout << *this << endl;
   for (int i=0; i<potmove->getSize(); i++ ) {
     cout << moveMap[blank]->getTiles()[i] << endl;
   }
+
+  hello
+  hello
+  hello
+  hello
+  hello
+  hello
 */
-          
+
 
 }
 
@@ -233,8 +240,8 @@ bool Board::solved() {
   for (int i=0; i< _size; i++) {
 
     if (_tiles[i] != i)
-    { return false; } 
-   
+    { return false; }
+
   }
   return true;
 }
@@ -260,7 +267,7 @@ bool Board::operator<(const Board& rhs) const {
 }
 
 ostream& operator<<(ostream &os, const Board &b) {
- 
+
   for (int i=0; i < sqrt(b._size); i++ ) {
   for (int j=i*sqrt(b._size); j<(i+1)*sqrt(b._size); j++) {
 
@@ -293,7 +300,7 @@ void Board::setTiles(int *tiles) {
 
   for (int i=0; i<_size; i++) {
 
-  _tiles[i] = tiles[i];  
+  _tiles[i] = tiles[i];
 
   }
 
